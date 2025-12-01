1b12988128d702521fb98956ba3d7bae51bcd2ea
mkaisheng <s10268302@connect.np.edu.sg>
2025-12-01 19:26:07 +0800

    add  condtional deployment

--- test_calculator.py
+++ test_calculator.py
@@ -3,12 +3,14 @@
 import pytest
 from calculator import add, subtract, multiply, divide,power
 
+@pytest.mark.edge
 def test_add():
     """Test addition function."""
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
 
+@pytest.mark.slow
 def test_subtract():
     """Test subtraction function."""
     assert subtract(5, 3) == 2
